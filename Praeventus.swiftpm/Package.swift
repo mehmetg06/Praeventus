11a1bd04343bab8637a1ed978bbd0120600f4400
Mehmet Gezoğlu <22030113@ogrenci.ankara.edu.tr>
2026-06-24 11:44:24 +0300

    Include adaptive water drop shape source

--- Praeventus.swiftpm/Package.swift
+++ Praeventus.swiftpm/Package.swift
@@ -12,6 +12,7 @@ let sources = [
     "WeatherStore.swift",
     "PraeventusRootView.swift",
     "AtmosphereBackgroundView.swift",
+    "WaterDropRoundedRectangle.swift",
     "GlassComponents.swift",
     "HomeView.swift",
     "WeatherLabView.swift",
